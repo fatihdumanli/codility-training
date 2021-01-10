66088fb23cdca0773f6b9136c1a31f9e4f2da7ce
Fatih Tunahan Dumanli <fatihdumanli@live.com>
2021-01-10 15:40:41 +0300

    missing integer and perm-check

---
+++ 10.perm-check/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _10.perm_check
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        static int solution(int[] A)
+        {
+            HashSet<int> hashSet = new HashSet<int>(A);
+
+            for(int i = 1; i <= A.Length; i++)
+            {
+                if (!hashSet.Contains(i))
+                    return 0;
+            }
+
+            return 1;
+        }
+    }
+}
---
+++ 9.missing integer/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _9.missing_integer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1, 3, 6, 4, 1, 2
+
+            var testArray = new int[6] { 1, 3, 6, 4, 1, 2 };
+
+            var result = solution(testArray);
+
+
+
+        }
+
+        static int solution(int[] A)
+        {
+            /*
+             * that, given an array A of N integers, returns the smallest positive integer
+             * (greater than 0) that does not occur in A.
+             */
+
+            HashSet<int> hashSet = new HashSet<int>(A);
+
+            int smallestPositiveInteger = 1;
+
+            for (int i = 0; i < A.Length; i++)
+            {
+                if(hashSet.Contains(smallestPositiveInteger))
+                {
+                    smallestPositiveInteger++;
+                    continue;
+                }    
+               
+            }
+
+            return smallestPositiveInteger;
+        }
+    }
+}
