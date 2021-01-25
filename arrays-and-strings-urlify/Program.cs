5c2bb2f83a9d6569184934e82ae26c01779b395b
Fatih Tunahan Dumanli <fatihdumanli@live.com>
2021-01-25 19:06:56 +0300

    string urlify

---
+++ arrays-and-strings-urlify/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace arrays_and_strings_urlify
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //For a given string, replace all spaces with %20.
+            //Stretch the string for a given actual length.
+            //INPUT : 'Mr John Smith       ', 13 (spaces counted.)
+            //OUTPUT: 'Mr%20John%20Smith'
+            var input = "Mr John Smith        ";
+
+            int actualLength = 13;
+
+            var urlfied = URLify(input, actualLength);
+
+            Console.WriteLine($"'{input}' is urlfied: '{urlfied}'");
+            Console.Read();
+        }
+
+        static string URLify(string phrase, int actualLength)
+        {
+            int spaceCount = 0;
+
+            for(int i = 0; i < actualLength; i++)
+            {
+                if (isSpace(phrase[i]))
+                    spaceCount++;
+            }
+
+            int spacesExcludedLength = actualLength - spaceCount;
+            // 3 * '%20'.Length.
+            int totalLength = spacesExcludedLength + (3 * spaceCount);
+
+            for (int i = 0; i < phrase.Length; i++)
+            {
+                if(isSpace(phrase[i]))
+                {
+                    phrase = phrase.Remove(i, 1);
+                    phrase = phrase.Insert(i, "%20");
+                }
+            }
+
+            return phrase.Substring(0, totalLength);
+        }
+
+        static bool isSpace(char c)
+        {
+            return (int)c == 32;
+        }
+    }
+}
---
+++ string-check-permutation/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace string_check_permutation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = "bccd", b = "cdcb";
+
+            var isPermAnother = isPermutation(a, b);
+            Console.WriteLine($"Is {a} a permutation of {b}: {isPermAnother}");
+            Console.Read();
+        }
+
+        static bool isPermutation(string a, string b)
+        {
+            if (a.Length != b.Length)
+                return false;
+
+            int length = a.Length; //or b.Length
+
+            //have same characters, in different order.
+            //an example:
+            //fatih - thifa
+
+            //use hashmap to find occurences.
+
+            Dictionary<char, int> aOccurences = new Dictionary<char, int>();
+            for (int i = 0; i < length; i++)
+            {
+                if (aOccurences.ContainsKey(a[i]))
+                {
+                    aOccurences[a[i]]++;
+                }
+
+                else
+                {
+                    aOccurences[a[i]] = 1;
+                }
+            }
+
+            Dictionary<char, int> bOccurences = new Dictionary<char, int>();
+            for (int i = 0; i < length; i++)
+            {
+                if (bOccurences.ContainsKey(b[i]))
+                {
+                    bOccurences[b[i]]++;
+                }
+
+                else
+                {
+                    bOccurences[b[i]] = 1;
+                }
+            }
+
+
+            //Means at least one dublication of a character, not a permutation.
+            if (aOccurences.Count != bOccurences.Count)
+                return false;
+
+            foreach(var pair in aOccurences)
+            {
+                if (!bOccurences.ContainsKey(pair.Key))
+                    return false;
+
+                if (bOccurences[pair.Key] != pair.Value)
+                    return false;
+            }
+
+            return true;
+        }
+    }
+}
