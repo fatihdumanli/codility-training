4624f853e3382295d1673c4e66d9048e7147760b
Fatih Tunahan Dumanli <fatihdumanli@live.com>
2021-01-25 18:21:14 +0300

    unique string

---
+++ is-unique-string/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace is_unique_string
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var phrase = "fatihf";
+            Console.WriteLine($"Does the string {phrase} consist of unique charactes: {isUnique(phrase)}");
+            Console.Read();
+        }
+
+        static bool isUnique(string phrase)
+        {
+            bool[] charMap = new bool[128];
+            bool isUnique = true;
+
+            foreach(char c in phrase)
+            {
+
+                if(charMap[c] == true)
+                {
+                    isUnique = false;
+                }
+
+                else
+                {
+                    charMap[c] = true;
+                }
+
+                if(!isUnique)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+  
+        }
+    }
+}
